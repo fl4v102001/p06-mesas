fd03749810e593435ec43c96a98a857b88b0a95a
fl4v102001 <flavio.sampaio@gmail.com>
2026-06-11 18:16:00 -0300

    constants from .env

--- frontend/src/constants.ts
+++ frontend/src/constants.ts
@@ -1,4 +1,2 @@
-//export const API_URL = 'http://192.168.0.198:8080';
-export const API_URL = 'https://p06-mesas.onrender.com';
-//export const WEBSOCKET_URL = 'ws://192.168.0.198:8080';
-export const WEBSOCKET_URL = 'ws://p06-mesas.onrender.com:8080';
+export const API_URL = process.env.REACT_APP_API_URL || 'http://localhost:8080';
+export const WEBSOCKET_URL = process.env.REACT_APP_WEBSOCKET_URL || 'ws://localhost:8080';
